da4e4a0cff808491b8f280b0c7fae305bfcf2433
mericcanusta <usta@stanford.edu>
2014-05-09 11:25:30 -0700

    Assignment 1 Submission
    Plot4 didn't look exactly as in the rubric

--- plot4.R
+++ plot4.R
@@ -23,7 +23,7 @@ data$Date<-as.POSIXct(paste(data$Date,data$Time),
 data <- data[,-2]
 png(file="plot4.png",width=480,height=480,units="px",bg="transparent")
 #Publish plot4.png
-par(mfrow=c(2,2),mar=c(4,4,2,1))
+par(mfrow=c(2,2))
 #Topleft plot-Time series of global active power
 with(data,plot(Date, Global_active_power, 
                type="n", 
